b153bce0c00b6c46613ea6ec0c82bbace48821ba
Aaditya Upreti <Aaditya Upreti>
2019-04-13 00:28:51 +0545

    Initial commit

---
+++ app/src/main/java/com/e/convertnumbersintofigures_aaditya/MainActivity_Aaditya.java
@@ -0,0 +1,55 @@
+package com.e.convertnumbersintofigures_aaditya;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity_Aaditya extends AppCompatActivity implements View.OnClickListener{
+
+    private EditText etNum;
+    private TextView tvOutput;
+    private Button btnConvert;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_aaditya);
+
+        btnConvert = findViewById(R.id.btnConvert);
+        etNum= findViewById(R.id.etNum);
+
+        tvOutput = findViewById(R.id.tvOutput);
+
+        btnConvert.setOnClickListener(this);
+    }
+
+
+    @Override
+    public void onClick(View v) {
+        if (v.getId() == R.id.btnConvert) {
+            if (validation()) {
+                String numberz =etNum.getText().toString();
+                final long number = Long.parseLong(numberz);
+                String returnz = Converter_Aaditya.convert(number);
+                tvOutput.setText(returnz);
+
+            }
+        }
+    }
+
+    public boolean validation(){
+        boolean flag = true;
+        if (TextUtils.isEmpty(etNum.getText().toString())){
+            etNum.setError("Please enter Height");
+            etNum.requestFocus();
+            flag = false;
+        }
+
+        return flag;
+    }
+}
+
